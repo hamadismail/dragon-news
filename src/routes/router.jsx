c8dde29335050dfcd1374a1fba11da2eab2d65cf
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-05-01 05:52:11 +0600

    peoject-setup

---
+++ src/routes/router.jsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from 'react-router';
+import HomeLayout from '../layouts/HomeLayout';
+
+const router = createBrowserRouter([{ path: '/', Component: HomeLayout }]);
+
+export default router;
