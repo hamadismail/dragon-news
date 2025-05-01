3917bac6814ac632dcfc1a92b15e9a1e107d6611
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-05-02 01:43:32 +0600

    hide-credentials

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDGzNTzI8O-bui_YhOJaFIrqexxpEFr6AQ',
-  authDomain: 'dragon-news-95b65.firebaseapp.com',
-  projectId: 'dragon-news-95b65',
-  storageBucket: 'dragon-news-95b65.firebasestorage.app',
-  messagingSenderId: '409540756878',
-  appId: '1:409540756878:web:f346fd7ee04befba2390df',
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
