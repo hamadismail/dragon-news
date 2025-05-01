4b9fbed927e4d68fce0f6fe891286c2c8ca06186
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-05-01 08:14:45 +0600

    implement rightSidebar

--- src/components/RightSideBar/QZone.jsx
+++ src/components/RightSideBar/QZone.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import swimming from '../../assets/images/swimming.png';
 import classes from '../../assets/images/class.png';
 import playground from '../../assets/images/playground.png';
+import rightBg from '../../assets/images/bg.png';
 
 const QZone = () => {
   return (
-    <div className="bg-base-200 mt-4 p-4">
-      <h2 className="text-lg text-accent font-semibold">QZone</h2>
+    <div className="bg-base-200 mt-4">
+      <div className="p-4">
+        <h2 className="text-lg text-accent font-semibold">QZone</h2>
 
-      <div className="pt-2 flex flex-col gap-4">
-        <img src={swimming} />
-        <img src={classes} />
-        <img src={playground} />
+        <div className="pt-2 flex flex-col gap-4">
+          <img src={swimming} />
+          <img src={classes} />
+          <img src={playground} />
+        </div>
       </div>
+      <img className="w-full" src={rightBg} />
     </div>
   );
 };
